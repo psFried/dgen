839cecf9db213ad0c1b2b30305148a3504721e5a
pfried <philipsfried@gmail.com>
2018-10-15 17:23:46 -0400

    add stable select to v2 interpreter

--- src/v2/builtins/select.rs
+++ src/v2/builtins/select.rs
@@ -1,10 +1,43 @@
 use failure::Error;
+use std::cell::RefCell;
 use std::fmt::Debug;
 use std::rc::Rc;
 use v2::{
     AnyFunction, Arguments, BuiltinFunctionPrototype, CreateFunctionResult, DataGenOutput, DynFun,
     FunctionPrototype, GenType, ProgramContext, RunnableFunction,
 };
+
+#[derive(Debug)]
+struct StableSelectFun<T> {
+    wrapped: Vec<DynFun<T>>,
+    index: RefCell<Option<usize>>,
+}
+
+impl<T> StableSelectFun<T> {
+    fn get_function(&self, ctx: &mut ProgramContext) -> DynFun<T> {
+        let StableSelectFun {
+            ref wrapped,
+            ref index,
+        } = *self;
+        let mut index = index.borrow_mut();
+        if index.is_none() {
+            *index = Some(ctx.gen_range(0, wrapped.len()));
+        }
+
+        wrapped[index.unwrap()].clone()
+    }
+}
+
+impl<T: Debug> RunnableFunction<T> for StableSelectFun<T> {
+    fn gen_value(&self, ctx: &mut ProgramContext) -> Result<T, Error> {
+        let fun = self.get_function(ctx);
+        fun.gen_value(ctx)
+    }
+    fn write_value(&self, ctx: &mut ProgramContext, out: &mut DataGenOutput) -> Result<u64, Error> {
+        let fun = self.get_function(ctx);
+        fun.write_value(ctx, out)
+    }
+}
 
 #[derive(Debug)]
 struct SelectFun<T> {
@@ -30,21 +63,41 @@ impl<T: Debug> RunnableFunction<T> for SelectFun<T> {
 const SELECT_ARG: &str = "gen";
 
 macro_rules! make_select_proto {
-    ($name:ident, $create_fn_name:ident, $gen_type:expr, $any_fun_type:path, $ret_type:ty, $convert_fun:path) => {
-        fn $create_fn_name(args: Arguments) -> CreateFunctionResult {
+    ($select_name:ident, $create_select_fn_name:ident, $stable_select_name:ident, $create_stable_select_fn_name:ident, $gen_type:expr, $any_fun_type:path, $ret_type:ty, $convert_fun:path) => {
+
+        fn $create_select_fn_name(args: Arguments) -> CreateFunctionResult {
             let as_types = args.get_required_varargs(SELECT_ARG, 0, $convert_fun)?;
             let sel = Rc::new(SelectFun { wrapped: as_types });
             let any = $any_fun_type(sel);
             Ok(any)
         }
 
-        pub const $name: &FunctionPrototype =
+        pub const $select_name: &FunctionPrototype =
             &FunctionPrototype::Builtin(&BuiltinFunctionPrototype {
                 function_name: "select",
                 description: "Randomly selects one of the input functions",
                 arguments: &[(SELECT_ARG, $gen_type)],
                 variadic: true,
-                create_fn: &$create_fn_name,
+                create_fn: &$create_select_fn_name,
+            });
+
+        fn $create_stable_select_fn_name(args: Arguments) -> CreateFunctionResult {
+            let as_types = args.get_required_varargs(SELECT_ARG, 0, $convert_fun)?;
+            let sel = Rc::new(StableSelectFun {
+                wrapped: as_types,
+                index: RefCell::new(None),
+            });
+            let any = $any_fun_type(sel);
+            Ok(any)
+        }
+
+        pub const $stable_select_name: &FunctionPrototype =
+            &FunctionPrototype::Builtin(&BuiltinFunctionPrototype {
+                function_name: "stable_select",
+                description: "Randomly selects one of the input functions and continues to select that same function forever",
+                arguments: &[(SELECT_ARG, $gen_type)],
+                variadic: true,
+                create_fn: &$create_stable_select_fn_name,
             });
     };
 }
@@ -52,6 +105,8 @@ macro_rules! make_select_proto {
 make_select_proto!(
     SELECT_CHAR_BUILTIN,
     create_select_char,
+    STABLE_SELECT_CHAR_BUILTIN,
+    create_stable_select_char,
     GenType::Char,
     AnyFunction::Char,
     char,
@@ -61,6 +116,8 @@ make_select_proto!(
 make_select_proto!(
     SELECT_STRING_BUILTIN,
     create_select_string,
+    STABLE_SELECT_STRING_BUILTIN,
+    create_stable_select_string,
     GenType::String,
     AnyFunction::String,
     IString,
@@ -70,6 +127,8 @@ make_select_proto!(
 make_select_proto!(
     SELECT_BOOLEAN_BUILTIN,
     create_select_boolean,
+    STABLE_SELECT_BOOLEAN_BUILTIN,
+    create_stable_select_boolean,
     GenType::Boolean,
     AnyFunction::Boolean,
     bool,
@@ -79,6 +138,8 @@ make_select_proto!(
 make_select_proto!(
     SELECT_UINT_BUILTIN,
     create_select_uint,
+    STABLE_SELECT_UINT_BUILTIN,
+    create_stable_select_uint,
     GenType::Uint,
     AnyFunction::Uint,
     u64,
@@ -88,6 +149,8 @@ make_select_proto!(
 make_select_proto!(
     SELECT_INT_BUILTIN,
     create_select_int,
+    STABLE_SELECT_INT_BUILTIN,
+    create_stable_select_int,
     GenType::Int,
     AnyFunction::Int,
     i64,
@@ -97,6 +160,8 @@ make_select_proto!(
 make_select_proto!(
     SELECT_DECIMAL_BUILTIN,
     create_select_decimal,
+    STABLE_SELECT_DECIMAL_BUILTIN,
+    create_stable_select_decimal,
     GenType::Decimal,
     AnyFunction::Decimal,
     f64,
@@ -106,6 +171,8 @@ make_select_proto!(
 make_select_proto!(
     SELECT_BIN_BUILTIN,
     create_select_bin,
+    STABLE_SELECT_BIN_BUILTIN,
+    create_stable_select_bin,
     GenType::Bin,
     AnyFunction::Bin,
     Vec<u8>,
