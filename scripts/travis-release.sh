062bc8642bde60162e4113cbd6137d6ce4440832
Phil Fried <philipsfried@gmail.com>
2018-12-27 16:15:45 -0500

    try uploading a directory for github release

--- scripts/travis-release.sh
+++ scripts/travis-release.sh
@@ -13,7 +13,8 @@ fi
 
 cargo build --release
 mkdir -p target/artifacts/${ARTIFACT_NAME}
+# remove any old artifacts if they are remaining
+rm -rf target/artifacts/*
+
 # for some reason, mv is giving an "are the same file" error so we'll just use cp for now
 cp "target/release/dgen${EXTENSION}" "target/artifacts/${ARTIFACT_NAME}/"
-
-zip -r "target/artifacts/${ARTIFACT_NAME}.zip" "target/artifacts/${ARTIFACT_NAME}/"
