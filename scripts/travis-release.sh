e552cdd9e972eec57e24e1c5be4db8811e709ecf
psFried <philipsfried@gmail.com>
2018-12-29 02:17:54 -0500

    one more try to get windows builds working

--- scripts/travis-release.sh
+++ scripts/travis-release.sh
@@ -6,10 +6,8 @@ cd "$(git rev-parse --show-toplevel)"
 
 ARTIFACT_NAME="dgen-${TRAVIS_TAG:-dev}-${TRAVIS_OS_NAME:-$OSTYPE}"
 
-COMMAND="zip"
 EXTENSION=""
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
-    COMMAND="7z"
     EXTENSION=".exe"
 fi
 
@@ -22,4 +20,8 @@ mkdir -p target/artifacts/${ARTIFACT_NAME}
 # for some reason, mv is giving an "are the same file" error so we'll just use cp for now
 cp "target/release/dgen${EXTENSION}" "target/artifacts/${ARTIFACT_NAME}/"
 
-eval ${COMMAND} -r target/artifacts/${ARTIFACT_NAME}.zip target/artifacts/${ARTIFACT_NAME}/
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+    7z -r target/artifacts/${ARTIFACT_NAME}.zip target/artifacts/${ARTIFACT_NAME}/
+else
+    zip -r target/artifacts/${ARTIFACT_NAME}.zip target/artifacts/${ARTIFACT_NAME}/
+fi
